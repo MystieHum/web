0a3b61b7967ec6e03ac366fd2f1ed2fb10657262
MystieHum <z232566755z@gmail.com>
2023-12-04 16:49:53 +0300

    foxhome!!! and some design changes of course

--- static/scripts/dialog.js
+++ static/scripts/dialog.js
@@ -1,8 +1,9 @@
 const dialogs = Array.from(document.querySelectorAll("dialog"));
-const showButtons = Array.from(document.querySelectorAll("dialog + button"));
+const showButtons = Array.from(document.querySelectorAll("button[data-dialog-open]"));
+const closeButtons = Array.from(document.querySelectorAll("button[data-dialog-close]"));
 dialogs.forEach((dialog, index) => {
 	const showButton = showButtons[index];
-	const closeButton = dialog.querySelector("button");
+	const closeButton = closeButtons[index];
 
 	showButton.addEventListener("click", () => {
 		dialog.showModal();
